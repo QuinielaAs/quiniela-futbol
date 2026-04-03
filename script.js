74f293a3586fc58ab2b06790528dc585d95a84a1
QuinielaAs <ramirezortizrobertocarlos1@gmail.com>
2026-04-03 00:41:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -450,3 +450,122 @@ hora
 });
 
 }
+
+function generarExcelGeneral(){
+
+db.ref("jugadores")
+.once("value", snapshot=>{
+
+let jugadores =
+snapshot.val();
+
+if(!jugadores){
+
+alert("No hay jugadores registrados");
+return;
+
+}
+
+let partidos =
+JSON.parse(
+localStorage.getItem("partidos")
+) || [];
+
+let datos = [];
+
+/* RECORRER JUGADORES */
+
+Object.keys(jugadores)
+.forEach(key=>{
+
+let j =
+jugadores[key];
+
+/* SOLO PAGADOS */
+
+if(j.pagado){
+
+let fila = {};
+
+fila["Nombre"] =
+j.nombre;
+
+fila["Combinaciones"] =
+j.combinaciones;
+
+fila["Total"] =
+j.total;
+
+/* PICKS */
+
+if(j.selecciones){
+
+j.selecciones.forEach((s,i)=>{
+
+if(s && s.length>0){
+
+let p = partidos[i];
+
+fila[
+p.l+" vs "+p.v
+] = s.join(",");
+
+}
+
+});
+
+}
+
+datos.push(fila);
+
+}
+
+});
+
+/* VALIDAR */
+
+if(datos.length == 0){
+
+alert("No hay jugadores PAGADOS");
+
+return;
+
+}
+
+/* CREAR EXCEL */
+
+let hoja =
+XLSX.utils.json_to_sheet(datos);
+
+let libro =
+XLSX.utils.book_new();
+
+XLSX.utils.book_append_sheet(
+libro,
+hoja,
+"Pagados"
+);
+
+/* SEMANA */
+
+let semana = 1;
+
+let input =
+document.getElementById("semana");
+
+if(input){
+
+semana = input.value || 1;
+
+}
+
+/* DESCARGAR */
+
+XLSX.writeFile(
+libro,
+"Quiniela_Pagados_Semana_"+semana+".xlsx"
+);
+
+});
+
+}
