7b21e83cf52cf728462010a76e82d93c851d71ec
QuinielaAs <ramirezortizrobertocarlos1@gmail.com>
2026-04-06 18:45:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -974,7 +974,9 @@ fila.push(v);
     
     sheet.addRow(fila);
     
-    numero++; }); 
+    numero++; 
+
+}); 
 
 }
     
@@ -1023,18 +1025,3 @@ alert("Error generando Excel");
     
 }
 
-/* ===========================
-FILA DE RESULTADOS
-=========================== */
-
-let filaResultados = 4;
-
-sheet.getCell(filaResultados,2).value = "RES";
-
-/* C hasta K = 9 partidos */
-
-for(let i=0;i<9;i++){
-
-sheet.getCell(filaResultados,3 + i).value = "";
-
-}
